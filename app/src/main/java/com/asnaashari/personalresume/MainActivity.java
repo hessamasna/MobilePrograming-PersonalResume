276422c61dc4ab13692417eae5e1a61ef19c4b7e
Hesam Asnaashari <hessamasna@yahoo.com>
2022-11-11 21:16:56 +0330

    [ADD] Theme Changer

--- app/src/main/java/com/asnaashari/personalresume/MainActivity.java
+++ app/src/main/java/com/asnaashari/personalresume/MainActivity.java
@@ -1,11 +1,14 @@
 package com.asnaashari.personalresume;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Switch themeSwitch = findViewById(R.id.switch_theme);
+        TextView themeTextView = findViewById(R.id.theme_textView);
+
+        themeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                themeTextView.setText("Dark Mode");
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                themeTextView.setText("Light Mode");
+
+            }
+
+        });
     }
 
     public void callBtn(View v) {
