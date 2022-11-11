7cdbb2e979b858051a22674052625ebd40ed5c38
Hesam Asnaashari <hessamasna@yahoo.com>
2022-11-11 17:44:30 +0330

    [ADD] call function

--- app/src/main/java/com/asnaashari/personalresume/MainActivity.java
+++ app/src/main/java/com/asnaashari/personalresume/MainActivity.java
@@ -2,7 +2,10 @@ package com.asnaashari.personalresume;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void callBtn(View v) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+98912"));
+        startActivity(intent);
     }
 }
